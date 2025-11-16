195bc3e74c4a0489f0a8dbf3da6c307d42dab68f
SleepyDroids <jacquelinelwrites@gmail.com>
2025-11-16 09:35:21 -0500

    minor UI changes

--- src/components/StarshipCard.jsx
+++ src/components/StarshipCard.jsx
@@ -7,17 +7,20 @@ export default function StarshipCard({ ship, addToCart }) {
   return (
     <>
       <div className="card">
-        <Link key={ship.url} to={`/info/${ship.name}`}>
-          <img src={reactLogo} alt={ship.name} />
-          <p><span className="ship-name">{ship.name}</span>
-             <br /> {ship.model}
-            <br />
-            ₹ {ship.cost_in_credits === "unknown"
-              ? "Contact dealer"
-              : ship.cost_in_credits}
-          </p>
-        </Link>
+        <img src={reactLogo} alt={ship.name} />
+        <p>
+          <span className="ship-name">{ship.name}</span>
+          <br /> {ship.model}
+          <br />₹{" "}
+          {ship.cost_in_credits === "unknown"
+            ? "Contact dealer"
+            : ship.cost_in_credits}
+        </p>
+
         <br />
+        <Link key={ship.url} to={`/info/${ship.name}`}>
+          <button>View Details</button>
+        </Link>
         <button onClick={() => addToCart(ship)}>Add to cart</button>
       </div>
     </>
